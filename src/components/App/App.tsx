3f9fd33949bcd7cf81fa59ee41d99daaf53a3eb3
Oksana <familianamy@gmail.com>
2025-09-21 09:21:41 +0300

    add file components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
-import type { Votes, VoteType } from '../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 import css from './App.module.css';
 
 export default function App() {
